e501ff930698bad899b358039399c393a05664e9
Bala Kumar <balaashok2004@gmail.com>
2025-10-21 14:23:04 +0530

    Intial commit

---
+++ src/test/java/com/bala/NotesManager/NotesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.NotesManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
